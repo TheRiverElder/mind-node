c319c7f7150d1c579eb05cdc8cd9afc1bbaa56aa
TheRiverElder <765375399@qq.com>
2020-08-23 22:11:12 +0800

    linkable nodes

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ function genId() {
 }
 
 function stopBubble(event) {
-    console.log(event);
     event.stopPropagation();
 }
 
+const POOL = document.getElementById('pool');
 const NODE_CONTAINER = document.getElementById('node-container');
+const LINK_CANVAS = document.getElementById('links');
+
+function resizeCanvas() {
+    LINK_CANVAS.width = POOL.offsetWidth;
+    LINK_CANVAS.height = POOL.offsetHeight;
+}
+
+window.addEventListener('resize', () => {
+    resizeCanvas();
+    redrawLinks();
+});
+
+
 
 const NODES = {};
 
@@ -23,6 +36,7 @@ const DRAG_STATE = {
 
 const LINK_STATE = {
     id: null,
+    linkEndCb: null,
 };
 
 NODE_CONTAINER.addEventListener('dragend', event => {
@@ -31,26 +45,64 @@ NODE_CONTAINER.addEventListener('dragend', event => {
         const y = event.offsetY - DRAG_STATE.startOffsetY;
         NODES[DRAG_STATE.id].moveTo(x, y);
         DRAG_STATE.id = null;
+        redrawLinks();
     }
 });
 
 
 
+function redrawLinks() {
+    const cxt = LINK_CANVAS.getContext('2d');
+    cxt.clearRect(0, 0, LINK_CANVAS.width, LINK_CANVAS.clientHeight);
+    cxt.strokeStyle = '#888';
+    cxt.lineWidth = 1.5;
+    for (let node of Object.values(NODES)) {
+        if (!node.outLinks.length) {
+            continue;
+        }
+        const fromPort = node.getPort();
+        for(let targetId of node.outLinks) {
+            const target = NODES[targetId];
+            const toPort = target.getPort();
+            const middle = (fromPort.outX + toPort.inX) / 2;
+            cxt.beginPath();
+            cxt.moveTo(fromPort.outX + 0.5, fromPort.outY + 0.5);
+            cxt.bezierCurveTo(middle, fromPort.outY, middle, toPort.inY, toPort.inX + 0.5, toPort.inY + 0.5);
+            cxt.stroke();
+        }
+    }
+}
 
 
-
+function link(from, to) {
+    if (from.outLinks.indexOf(to.id) < 0) {
+        from.outLinks.push(to.id);
+    }
+    if (to.inLinks.indexOf(from.id) < 0) {
+        to.inLinks.push(from.id);
+    }
+    redrawLinks();
+}
 
 
 function createNode() {
     const node = {
         id: genId(),
         el: null,
+        inLinks: [],
+        outLinks: [],
         moveTo(x, y) {
             this.el.style.left = x + 'px';
             this.el.style.top = y + 'px';
         },
         getPort() {
-            
+            const el = this.el;
+            return {
+                inX: el.offsetLeft,
+                inY: el.offsetTop + 20,
+                outX: el.offsetLeft + el.offsetWidth,
+                outY: el.offsetTop + 20,
+            };
         },
     };
 
@@ -71,8 +123,8 @@ function createNodeEl(node) {
 
     const content = Object.assign(document.createElement('div'), {className: 'content pa-p5'});
     const txtContent = Object.assign(document.createElement('span'), {innerText: 'TEXT'});
-    const iptContent = Object.assign(document.createElement('textarea'), {className: 'hidden'});
-    iptContent.addEventListener('mousedown', stopBubble);
+    const iptContent = Object.assign(document.createElement('textarea'), {className: 'hidden', draggable: false});
+    // iptContent.addEventListener('mousedown', stopBubble);
     content.appendChild(txtContent);
     content.appendChild(iptContent);
     el.appendChild(content);
@@ -80,8 +132,8 @@ function createNodeEl(node) {
     const actionBar = Object.assign(document.createElement('div'), {className: 'node-button-bar fill-width d-flex align-items-center'});
     const btnEditOrDone = Object.assign(document.createElement('button'), {className: 'flex-grow-1', innerText: 'Edit'});
     const btnDelete = Object.assign(document.createElement('button'), {className: 'flex-grow-1', innerText: 'Delete'});
-    const hdlLink = Object.assign(document.createElement('div'), {className: 'node-handle'});
-    hdlLink.addEventListener('mousedown', stopBubble);
+    const hdlLink = Object.assign(document.createElement('div'), {className: 'node-handle', draggable: false});
+    // hdlLink.addEventListener('mousedown', stopBubble);
     actionBar.appendChild(btnEditOrDone);
     actionBar.appendChild(btnDelete);
     actionBar.appendChild(hdlLink);
@@ -103,6 +155,24 @@ function createNodeEl(node) {
         }
     });
 
+    hdlLink.addEventListener('click', () => {
+        if (LINK_STATE.id === node.id) {
+            LINK_STATE.id = null;
+            LINK_STATE.linkEndCb = null;
+            hdlLink.classList.remove('linking');
+        } else if (LINK_STATE.id === null) {
+            LINK_STATE.id = node.id;
+            LINK_STATE.linkEndCb = () => hdlLink.classList.remove('linking');
+            hdlLink.classList.add('linking');
+        } else {
+            link(NODES[LINK_STATE.id], node);
+            LINK_STATE.linkEndCb();
+            LINK_STATE.id = null;
+            LINK_STATE.linkEndCb = null;
+            hdlLink.classList.remove('linking');
+        }
+    });
+
     el.addEventListener('dragstart', event => {
         DRAG_STATE.id = node.id;
         DRAG_STATE.startOffsetX = event.offsetX - el.offsetLeft;
@@ -118,3 +188,5 @@ function createAndAppendNode() {
 }
 
 createAndAppendNode();
+resizeCanvas();
+redrawLinks();
